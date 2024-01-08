fd5f2a8d3c074fc7715cbb0b885768fdcfaa09ca
Faridzil <faridmusica25@gmail.com>
2024-01-08 14:43:55 -0600

    first commit

---
+++ src/main/java/com/lbo_soft/consultorio/apiconsultorio/BackendConsultorioApplication.java
@@ -0,0 +1,13 @@
+package com.lbo_soft.consultorio.apiconsultorio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendConsultorioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendConsultorioApplication.class, args);
+	}
+
+}
